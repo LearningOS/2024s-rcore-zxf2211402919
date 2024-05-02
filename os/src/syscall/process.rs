7a65b53a25b8a11786c8eb387f7cfd0b1f70dc01
zxf2211402919 <18166435770@163.com>
2024-05-02 09:25:35 +0800

    ch3

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -4,7 +4,8 @@ use crate::{
     task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus},
     timer::get_time_us,
 };
-
+use crate::task::get_sys_call_times;
+use crate::timer::get_time_ms;
 #[repr(C)]
 #[derive(Debug)]
 pub struct TimeVal {
@@ -53,5 +54,12 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
     trace!("kernel: sys_task_info");
-    -1
+    unsafe {
+        *_ti = TaskInfo {
+            status: TaskStatus::Running,
+            syscall_times: get_sys_call_times(),
+            time: get_time_ms(),
+        }
+    }
+    0
 }
